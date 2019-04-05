6dd18fcf52c9e64bc494ce3e0f75a4cd677df5bc
Soumil Khandelwal <31521503+soumilk@users.noreply.github.com>
2019-04-05 21:25:16 +0530

    Program without main() !!!

---
+++ Macros and wonders.cpp
@@ -0,0 +1,19 @@
+/* the main() function in the C++ program is the core driver function and
+ without this function the code will not proceed.
+ Is there any way we can substitute the main function!!??
+YEAH, there is:
+ */
+ // We all know the general working of marcos, this is the example which is unique and
+ // is tried by very less number of people
+ #include<iostream>
+ using namespace std;
+ # define soumil main
+
+ int  soumil()
+ {
+   cout<<"This is the main function"<<endl;
+   return 0;
+ }
+
+/* So now if anyone ask that can we call the main function without actually calling
+the function with name main?!  then answer is yes!! */
