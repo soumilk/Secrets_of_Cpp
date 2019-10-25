23b6c24b91aea530d60e1dedce26723c1c9cbf9d
Neha <nehaguptang.ng@gmail.com>
2019-10-25 14:35:14 +0530

    Add common factor

---
+++ C++boost/Commonfactor.cpp
@@ -0,0 +1,15 @@
+
+
+// CPP program to illustrate illegal 
+// behaviour of boost::math::lcm function of C++  
+#include <iostream> 
+#include <boost/math/common_factor.hpp> 
+  
+using namespace std; 
+  
+int main() 
+{ 
+    cout << "LCM(1.0,20) = " << boost::math::lcm(1.0,20)  
+         << endl; 
+    return 0; 
+} 
